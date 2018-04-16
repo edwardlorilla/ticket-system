66d27f35cfae3e74e85c1cf4b4ac3b90419ddc1e
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2018-04-16 13:07:58 +0800

    validate the fhorm with vee validate

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import VeeValidate from 'vee-validate';
 import SubmitTicketFormComponent from './components/SubmitTicketFormComponent.vue';
 
 
 require('./bootstrap');
 
 window.Vue = Vue;
+
+Vue.use(VeeValidate);
 
 const app = new Vue({
     el: '#app',
